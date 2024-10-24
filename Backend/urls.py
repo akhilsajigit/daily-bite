a2a937a1068476afca48699abd41aa29b97649af
akhilsajigit <akhilsaji5470@gmail.com>
2024-10-24 23:32:06 +0530

    Dailybite depolyed for improvements

--- Backend/urls.py
+++ Backend/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from Backend import views
 
 urlpatterns = [
-  path('',views.admin_page, name="admin_page")
+  path('admin_page/',views.admin_page, name="admin_page")
 ]
--- DailyBite/urls.py
+++ DailyBite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Backend.urls')),
+    path('Backend/', include('Backend.urls')),
     path('Frontend/', include('Frontend.urls'))
 ]
