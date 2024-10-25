d12b57c7f4c116021e2b6c4197fa126f52069281
akhilsajigit <akhilsaji5470@gmail.com>
2024-10-25 20:20:52 +0530

    Sample commit

--- Frontend/urls.py
+++ Frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Frontend import views
 
 urlpatterns = [
-  path('index/', views.index_page, name="index"),
+  path('', views.index_page, name="index"),
   path('Menu/', views.menu_page, name="Menu"),
   path('About/', views.about_page, name="About"),
   path('Booking/', views.booking_page, name="Booking"),
