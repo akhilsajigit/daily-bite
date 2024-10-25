d3e280b752de13f12b123224a2b953003d904ac1
akhilsajigit <akhilsaji5470@gmail.com>
2024-10-25 22:02:31 +0530

    Updated Backend menu templates

--- Backend/urls.py
+++ Backend/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from Backend import views
 
 urlpatterns = [
-  path('admin_page/',views.admin_page, name="admin_page")
+  path('',views.admin_page, name="admin_page"),
+  path('add_menu_page/',views.add_menu_page, name="add_menu_page"),
+  path('view_menu/', views.view_menu, name="view_menu"),
+  path('add_items/', views.add_items, name="add_items"),
+  path('view_items/', views.view_items, name="view_items"),
 ]
--- Backend/views.py
+++ Backend/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def admin_page(request):
   return render(request, "admin-page.html")
+
+def add_menu_page(request):
+  return render(request, "add-menu.html")
+
+def view_menu(request):
+  return render(request, "view-menu.html")
+
+def add_items(request):
+  return render(request, "add-items.html")
+
+def view_items(request):
+  return render(request, "view-items.html")
